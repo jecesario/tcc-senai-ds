7c48a085b0078954df1f4db652fb050c4bf1e58b
jecesario <jp@soloid.com.br>
2021-09-13 22:18:00 -0300

    Iniciando model de curriculo

--- backend/Models/Curriculo.cs
+++ backend/Models/Curriculo.cs
@@ -8,18 +8,14 @@ namespace backend.Models
     public class Curriculo
     {
         public int Id { get; set; }
-        public string Aluno { get; set; }
-
-        public static List<Curriculo> listar()
-        {
-            var lista = new List<Curriculo>();
-            var curriculo = new Curriculo();
-            curriculo.Id = 1;
-            curriculo.Aluno = "Eduardo";
-            lista.Add(curriculo);
-            
-
-            return lista;
-        }
+        public string Github { get; set; }
+        public string Linkedin { get; set; }
+        public string Telefone { get; set; }
+        public string Resumo { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public Usuario Usuario { get; set; }
+        public Curso Curso { get; set; }
     }
 }
