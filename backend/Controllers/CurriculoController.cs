5b7c93c12ec703814388d5021bb6180327a3cb26
jecesario <jp@soloid.com.br>
2021-09-15 20:29:27 -0300

    Criando FrontEnd de cadastrar dados iniciais do curriculo

--- backend/Controllers/CurriculoController.cs
+++ backend/Controllers/CurriculoController.cs
@@ -16,12 +16,44 @@ namespace backend.Controllers
             {
                 return RedirectToAction("Entrar", "Home");
             }
-            var curriculos = Curriculo.listar();
-            if (curriculos == null || curriculos.Count == 0)
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo != 1)
             {
-                TempData["alertInfo"] = "Epa, perai! Parece que não tem nenhum curso cadastrado!";
+                return RedirectToAction("Entrar", "Home");
             }
-            return View(curriculos);
+            //var curriculos = Curriculo.listar();
+            //if (curriculos == null || curriculos.Count == 0)
+            //{
+            //    TempData["alertInfo"] = "Epa, perai! Parece que não tem nenhum curso cadastrado!";
+            //}
+            return View();
+        }
+
+        public ActionResult MeuCurriculo() {
+            //if (Session["usuario"] == null)
+            //{
+            //    return RedirectToAction("Entrar", "Home");
+            //}
+            //var usuario = Session["usuario"] as Usuario;
+            //if (usuario.Tipo != 1)
+            //{
+            //    return RedirectToAction("Index", "Curriculo");
+            //}
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            //if (Session["usuario"] == null)
+            //{
+            //    return RedirectToAction("Entrar", "Home");
+            //}
+            //var usuario = Session["usuario"] as Usuario;
+            //if (usuario.Tipo != 1)
+            //{
+            //    return RedirectToAction("Index", "Curriculo");
+            //}
+            return View();
         }
     }
 }
