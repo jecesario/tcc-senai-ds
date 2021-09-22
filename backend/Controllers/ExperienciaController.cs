dc45b2d6dd1c8608ee55f023c070bbc4cc7e4905
jecesario <jp@soloid.com.br>
2021-09-21 22:18:47 -0300

    WIP - Cadastro de Experiencias - Cadastrando no banco de dados, falta exiber na pagina Meu Curriculo e tirar mensagem de alerta

---
+++ backend/Controllers/ExperienciaController.cs
@@ -0,0 +1,56 @@
+﻿using backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace backend.Controllers
+{
+    public class ExperienciaController : Controller
+    {
+        // GET: Experiencia
+        public ActionResult Cadastrar()
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult CadastrarAction()
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+
+            // Guardando dados do curriculo do usuário logado 
+            var curriculo = new Curriculo();
+            curriculo.UsuarioId = usuario.Id.ToString();
+            curriculo = curriculo.buscarPorUsuarioId();
+
+            var experiencia = new Experiencia();
+            experiencia.Cargo = Request.Form["cargo"];
+            experiencia.Empregador = Request.Form["empregador"];
+            experiencia.Resumo = Request.Form["resumo"];
+            experiencia.Admissao = Request.Form["admissao"];
+            experiencia.Demissao = Request.Form["demissao"];
+            experiencia.CurriculoId = curriculo.Id;
+            experiencia.cadastrar();
+            TempData["alertSucesso"] = "Experiencia adicionada com sucesso!";
+            return RedirectToAction("MeuCurriculo", "Curriculo");
+        }
+    }
+}
---
+++ backend/Models/Experiencia.cs
@@ -0,0 +1,54 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Experiencia
+    {
+        private static string dbConfig = ConfigurationManager.ConnectionStrings["dbConfigSenai"].ConnectionString;
+        public int Id { get; set; }
+        public string Cargo { get; set; }
+        public string Empregador { get; set; }
+        public string Resumo { get; set; }
+        public string Admissao { get; set; }
+        public string Demissao { get; set; }
+        public int CurriculoId { get; set; }
+
+        public bool cadastrar()
+        {
+            var con = new MySqlConnection(dbConfig);
+            bool resp = false;
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "INSERT INTO experiencias (cargo, empregador, resumo, admissao, demissao, curriculo_id) VALUES (@cargo, @empregador, @resumo, @admissao, @demissao, @curriculoId)";
+                query.Parameters.AddWithValue("@cargo", Cargo);
+                query.Parameters.AddWithValue("@empregador", Empregador);
+                query.Parameters.AddWithValue("@resumo", Resumo);
+                query.Parameters.AddWithValue("@admissao", Admissao);
+                query.Parameters.AddWithValue("@demissao", Demissao);
+                query.Parameters.AddWithValue("@curriculoId", CurriculoId);
+                if (query.ExecuteNonQuery() > 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (Exception e)
+            {
+                resp = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return resp;
+        }
+    }
+}
