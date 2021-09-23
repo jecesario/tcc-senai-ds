a4536e051b14c5154e8a186fad9a1a4cf1e6772f
jecesario <jp@soloid.com.br>
2021-09-22 22:18:46 -0300

    Crud de experiencias completo

--- backend/Controllers/ExperienciaController.cs
+++ backend/Controllers/ExperienciaController.cs
@@ -48,8 +48,89 @@ namespace backend.Controllers
             experiencia.Admissao = Request.Form["admissao"];
             experiencia.Demissao = Request.Form["demissao"];
             experiencia.CurriculoId = curriculo.Id;
-            experiencia.cadastrar();
-            TempData["alertSucesso"] = "Experiencia adicionada com sucesso!";
+            if (experiencia.cadastrar())
+            {
+                TempData["alertSucesso"] = "Experiencia editada com sucesso!";
+            }
+            else
+            {
+                TempData["alertErro"] = "Ocorreu um erro ao editar Experiencia!";
+            }
+            return RedirectToAction("MeuCurriculo", "Curriculo");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var experiencia = new Experiencia();
+            experiencia.Id = id;
+            return View(experiencia.buscarPorId());
+        }
+
+        public ActionResult EditarAction()
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+
+            // Guardando dados do curriculo do usuário logado 
+            var curriculo = new Curriculo();
+            curriculo.UsuarioId = usuario.Id.ToString();
+            curriculo = curriculo.buscarPorUsuarioId();
+
+            var experiencia = new Experiencia();
+            experiencia.Cargo = Request.Form["cargo"];
+            experiencia.Empregador = Request.Form["empregador"];
+            experiencia.Resumo = Request.Form["resumo"];
+            experiencia.Admissao = Request.Form["admissao"];
+            experiencia.Demissao = Request.Form["demissao"];
+            experiencia.CurriculoId = curriculo.Id;
+            experiencia.Id = int.Parse(Request.Form["id"]);
+            if(experiencia.editar())
+            {
+                TempData["alertSucesso"] = "Experiencia editada com sucesso!";
+            } else
+            {
+                TempData["alertErro"] = "Ocorreu um erro ao editar Experiencia!";
+            }
+            return RedirectToAction("MeuCurriculo", "Curriculo");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var experiencia = new Experiencia();
+            experiencia.Id = id;
+            if (experiencia.apagar())
+            {
+                TempData["alertSucesso"] = "Experiencia apagada com sucesso!";
+            }
+            else
+            {
+                TempData["alertErro"] = "Ocorreu um erro ao deletar Experiencia!";
+            }
             return RedirectToAction("MeuCurriculo", "Curriculo");
         }
     }
--- backend/Models/Experiencia.cs
+++ backend/Models/Experiencia.cs
@@ -16,8 +16,6 @@ namespace backend.Models
         public string Resumo { get; set; }
         public string Admissao { get; set; }
         public string Demissao { get; set; }
-        public DateTime Admissao2 { get; set; }
-        public DateTime Demissao2 { get; set; }
         public int CurriculoId { get; set; }
 
         public bool cadastrar()
@@ -96,6 +94,111 @@ namespace backend.Models
             }
 
             return experiencias;
+        }
+
+        public bool apagar()
+        {
+            var con = new MySqlConnection(dbConfig);
+            bool resp = false;
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "DELETE FROM experiencias WHERE id = @id";
+                query.Parameters.AddWithValue("@id", Id);
+                if (query.ExecuteNonQuery() > 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (Exception e)
+            {
+                resp = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return resp;
+        }
+
+        public bool editar()
+        {
+            var con = new MySqlConnection(dbConfig);
+            bool resp = false;
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "UPDATE experiencias SET cargo = @cargo, empregador = @empregador, resumo = @resumo, admissao = @admissao, demissao = @demissao, curriculo_id = @curriculoId WHERE id = @id";
+                query.Parameters.AddWithValue("@cargo", Cargo);
+                query.Parameters.AddWithValue("@empregador", Empregador);
+                query.Parameters.AddWithValue("@resumo", Resumo);
+                query.Parameters.AddWithValue("@admissao", Admissao);
+                query.Parameters.AddWithValue("@demissao", Demissao);
+                query.Parameters.AddWithValue("@curriculoId", CurriculoId);
+                query.Parameters.AddWithValue("@id", Id);
+                if (query.ExecuteNonQuery() > 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (Exception e)
+            {
+                resp = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return resp;
+        }
+
+        public Experiencia buscarPorId()
+        {
+            var con = new MySqlConnection(dbConfig);
+            var experiencia = new Experiencia();
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "SELECT * FROM experiencias WHERE id = @id";
+                query.Parameters.AddWithValue("@id", Id);
+                var dados = query.ExecuteReader();
+
+                if (dados.HasRows)
+                {
+                    while (dados.Read())
+                    {
+                        experiencia.Id = dados.GetInt32("id");
+                        experiencia.Cargo = dados.GetString("cargo");
+                        experiencia.Empregador = dados.GetString("empregador");
+                        experiencia.Resumo = dados.GetString("resumo");
+                        experiencia.Admissao = dados.GetDateTime("admissao").ToString("dd/MM/yyyy");
+                        experiencia.Demissao = dados.GetDateTime("demissao").ToString("dd/MM/yyyy");
+                    }
+                }
+                else
+                {
+                    experiencia = null;
+                }
+
+            }
+            catch (Exception e)
+            {
+                experiencia = null;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return experiencia;
         }
 
     }
