a48fb828e95d6710d114e02bd043fe9db0643af3
jecesario <jp@soloid.com.br>
2021-09-23 20:18:07 -0300

    Crud de formação completo

--- backend/Controllers/FormacaoController.cs
+++ backend/Controllers/FormacaoController.cs
@@ -59,5 +59,83 @@ namespace backend.Controllers
             }
             return RedirectToAction("MeuCurriculo", "Curriculo");
         }
+
+        public ActionResult Editar(int id)
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var formacao = new Formacao();
+            formacao.Id = id;
+            ViewBag.Formacao = formacao.buscarPorId();
+            return View(TiposFormacao.listar());
+        }
+
+        public ActionResult EditarAction()
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+
+            // Guardando dados do curriculo do usuário logado 
+            var curriculo = new Curriculo();
+            curriculo.UsuarioId = usuario.Id.ToString();
+            curriculo = curriculo.buscarPorUsuarioId();
+
+            var formacao = new Formacao();
+            formacao.Nome = Request.Form["nome"];
+            formacao.Instituicao = Request.Form["instituicao"];
+            formacao.Inicio = Request.Form["inicio"];
+            formacao.Conclusao = Request.Form["conclusao"];
+            formacao.Resumo = Request.Form["resumo"];
+            formacao.CurriculoId = curriculo.Id;
+            formacao.TipoFormacaoId = int.Parse(Request.Form["tipoFormacao"]);
+            formacao.Id = int.Parse(Request.Form["id"]);
+            if (formacao.editar())
+            {
+                TempData["alertSucesso"] = "Formação editada com sucesso!";
+            }
+            else
+            {
+                TempData["alertErro"] = "Ocorreu um erro ao editar Formação!";
+            }
+            return RedirectToAction("MeuCurriculo", "Curriculo");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            if (Session["usuario"] == null)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo == 1)
+            {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var formacao = new Formacao();
+            formacao.Id = id;
+            if (formacao.apagar())
+            {
+                TempData["alertSucesso"] = "Formação apagada com sucesso!";
+            }
+            else
+            {
+                TempData["alertErro"] = "Ocorreu um erro ao deletar Formação!";
+            }
+            return RedirectToAction("MeuCurriculo", "Curriculo");
+        }
     }
 }
--- backend/Models/Formacao.cs
+++ backend/Models/Formacao.cs
@@ -98,5 +98,112 @@ namespace backend.Models
 
             return formacoes;
         }
+
+        public bool apagar()
+        {
+            var con = new MySqlConnection(dbConfig);
+            bool resp = false;
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "DELETE FROM formacao WHERE id = @id";
+                query.Parameters.AddWithValue("@id", Id);
+                if (query.ExecuteNonQuery() > 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (Exception e)
+            {
+                resp = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return resp;
+        }
+
+        public bool editar()
+        {
+            var con = new MySqlConnection(dbConfig);
+            bool resp = false;
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "UPDATE formacao SET nome = @nome, instituicao = @instituicao, inicio = @inicio, conclusao = @conclusao, resumo = @resumo, curriculo_id = @curriculoId, tipo_formacao_id = @tipoFormacaoId WHERE id = @id";
+                query.Parameters.AddWithValue("@nome", Nome);
+                query.Parameters.AddWithValue("@instituicao", Instituicao);
+                query.Parameters.AddWithValue("@inicio", Inicio);
+                query.Parameters.AddWithValue("@conclusao", Conclusao);
+                query.Parameters.AddWithValue("@resumo", Resumo);
+                query.Parameters.AddWithValue("@curriculoId", CurriculoId);
+                query.Parameters.AddWithValue("@tipoFormacaoId", TipoFormacaoId);
+                query.Parameters.AddWithValue("@id", Id);
+                if (query.ExecuteNonQuery() > 0)
+                {
+                    resp = true;
+                }
+            }
+            catch (Exception e)
+            {
+                resp = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return resp;
+        }
+
+        public Formacao buscarPorId()
+        {
+            var con = new MySqlConnection(dbConfig);
+            var formacao = new Formacao();
+
+            try
+            {
+                con.Open();
+                var query = con.CreateCommand();
+                query.CommandText = "SELECT * FROM formacao WHERE id = @id";
+                query.Parameters.AddWithValue("@id", Id);
+                var dados = query.ExecuteReader();
+
+                if (dados.HasRows)
+                {
+                    while (dados.Read())
+                    {
+                        formacao.Id = dados.GetInt32("id");
+                        formacao.Nome = dados.GetString("nome");
+                        formacao.Instituicao = dados.GetString("instituicao");
+                        formacao.Inicio = dados.GetDateTime("inicio").ToString("dd/MM/yyyy");
+                        formacao.Conclusao = dados.GetDateTime("conclusao").ToString("dd/MM/yyyy");
+                        formacao.Resumo = dados.GetString("resumo");
+                        formacao.TipoFormacaoId = dados.GetInt32("tipo_formacao_id");
+                    }
+                }
+                else
+                {
+                    formacao = null;
+                }
+
+            }
+            catch (Exception e)
+            {
+                formacao = null;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return formacao;
+        }
     }
 }
