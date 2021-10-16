14e2caaf55e49388e06ba0a64ae274b48d567694
jecesario <jp@soloid.com.br>
2021-10-15 21:54:03 -0300

    Adicionado controller e view de cadastrar e editar tipos de formação

--- backend/Controllers/TipoFormacaoController.cs
+++ backend/Controllers/TipoFormacaoController.cs
@@ -25,5 +25,61 @@ namespace backend.Controllers
             }
             return View(tipoFormacao);
         }
+
+        public ActionResult Cadastrar() {
+            if (Session["usuario"] == null) {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo != 1) {
+                return RedirectToAction("Entrar", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarAction() {
+            var tipoFormacao = new TipoFormacao();
+            tipoFormacao.Nome = Request.Form["nome"];
+            if (tipoFormacao.cadastrar()) {
+                TempData["alertSucesso"] = "Tipo de formação cadastrado com sucesso!";
+            }
+            else {
+                TempData["alertErro"] = "Ocorreu um erro ao cadastrar tipo de formação!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id) {
+            if (Session["usuario"] == null) {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var usuario = Session["usuario"] as Usuario;
+            if (usuario.Tipo != 1) {
+                return RedirectToAction("Entrar", "Home");
+            }
+            var tipoFormacao = new TipoFormacao();
+            tipoFormacao.Id = id;
+
+            return View(tipoFormacao.buscarPorId());
+        }
+
+        public ActionResult EditarAction() {
+            var nome = Request.Form["nome"];
+            var id = int.Parse(Request.Form["id"]);
+
+            var tipoFormacao = new TipoFormacao();
+            tipoFormacao.Id = id;
+            tipoFormacao.Nome = nome;
+            if (tipoFormacao.editar()) {
+                TempData["alertSucesso"] = "Tipo de formação editado com sucesso!";
+            }
+            else {
+                TempData["alertErro"] = "Ocorreu um erro ao editar tipo de formação!";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
