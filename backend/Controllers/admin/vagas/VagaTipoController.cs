d0bc299eeeaea7243f4e35bc5f3d4efae3e52e00
jecesario <jp@soloid.com.br>
2021-10-21 19:53:55 -0300

    Criada model de tipo de vaga

---
+++ backend/Controllers/admin/vagas/VagaTipoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace backend.Controllers.admin.vagas
+{
+    public class VagaTipoController : Controller
+    {
+        // GET: VagaTipo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
