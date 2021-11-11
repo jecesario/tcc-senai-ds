4863e820ce336d7001c6b977f7db2082c7f84a73
jecesario <jp@soloid.com.br>
2021-11-11 19:22:30 -0300

    Criado o método de utilidade para criptografar strings

---
+++ backend/Models/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Util
+    {
+        static string criptografar(string value)
+        {
+            var UE = new UnicodeEncoding();
+            byte[] HashValue, MessagesBytes = UE.GetBytes(value);
+            var SHhash = new SHA256Managed();
+            string strhex = "";
+
+            HashValue = SHhash.ComputeHash(MessagesBytes);
+            foreach (byte b in HashValue)
+            {
+                strhex += String.Format("{0:x2}", b);
+            }
+            return strhex;
+        }
+    }
+}
